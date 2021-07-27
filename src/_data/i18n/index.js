d1c99e1ae7750cdd4022bc6c7612fd8a0b0fff6f
Charles De Mount <thexrayone@icloud.com>
2021-07-27 19:41:24 -0400

    fix: :fire: remove code issues

--- src/_data/i18n/index.js
+++ src/_data/i18n/index.js
@@ -1,77 +1,29 @@
 module.exports = {
   navigation: {
-    values: {
-      en: 'values',
-      fr: 'valeurs',
+    about: {
+      en: 'About Us',
+      fr: 'À Propos',
     },
-    focus: {
-      en: 'focus',
-      fr: 'priorités',
+    mission: {
+      en: 'Our Mission',
+      fr: 'Notre Mission',
     },
-    tools: {
-      en: 'tools',
-      fr: 'outils',
+    contact: {
+      en: 'Contact Us',
+      fr: 'Contactez Nous',
     },
-    careers: {
-      en: 'careers',
-      fr: 'carrières',
+    join: {
+      en: 'Join the Team',
+      fr: 'Joignez l‘Équipe',
     },
   },
   lang: {
-    en: 'français',
-    fr: 'english',
+    en: 'Français',
+    fr: 'English',
   },
   cta: {
-    en: 'Get in touch',
-    fr: 'Rentrez en contact',
-  },
-  values: {
-    title: {
-      en: 'Why companies choose us',
-      fr: 'Nos valeurs principales',
-    },
-    detail: {
-      en: 'Detailed',
-      fr: 'Méticuleux',
-    },
-    experience: {
-      en: 'Experienced',
-      fr: 'Chevronnés',
-    },
-    focus: {
-      en: 'Focused',
-      fr: 'Attentifs',
-    },
-    transparent: {
-      en: 'Transparent',
-      fr: 'Transparents',
-    },
-    inter: {
-      en: 'International',
-      fr: 'Multilingues',
-    },
-    results: {
-      en: 'Result–Oriented',
-      fr: 'Gagnants',
-    },
-  },
-  focus: {
-    product: {
-      en: 'Focus on your product',
-      fr: 'Axés sur votre produit',
-    },
-    progress: {
-      en: 'Follow your progress',
-      fr: 'Suivez votre progrès',
-    },
-  },
-  tools: {
-    en: 'Our Tools',
-    fr: 'Nos Outils',
-  },
-  clients: {
-    en: 'Case Studies',
-    fr: "Cas d'Études",
+    en: 'Apply Now',
+    fr: 'Appliquez Maintenant',
   },
   form: {
     title: {
