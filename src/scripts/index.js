22a1c7e766842c06f739f28c9ba6601fca71701c
Charles De Mount <thexrayone@icloud.com>
2021-07-29 17:30:46 -0400

    feat(modal): :tada: wire up modal logic

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -14,12 +14,13 @@
 //   validateForm(mainForm, lang);
 // }
 
+/** Menu Open and Close */
 const bodyEl = document.querySelector('body');
 const menuToggle = document.querySelector('#menu-toggle');
 const menu = document.querySelector('#menu');
 
 menuToggle.addEventListener('click', (e) => {
-  bodyEl.classList.toggle('is-menu-open');
+  bodyEl.classList.toggle('is-modal-open');
   const isMenuVisible = menuToggle.getAttribute('aria-expanded') == 'true';
   const newMenuState = !isMenuVisible;
 
@@ -33,10 +34,33 @@ menuToggle.addEventListener('click', (e) => {
 const menuItems = menu.querySelector('ul');
 
 menuItems.addEventListener('click', (e) => {
-  bodyEl.classList.remove('is-menu-open');
+  bodyEl.classList.remove('is-modal-open');
 
   menuToggle.setAttribute('aria-expanded', false);
   menu.setAttribute('aria-hidden', true);
   menuToggle.setAttribute('data-inverted', false);
   menuToggle.textContent = 'Menu';
 });
+
+/* Modal Logic */
+const modalEl = document.querySelector('[role="dialog"]');
+const modalToggle = document.querySelector('#modal-toggle');
+
+modalToggle.addEventListener('click', () => {
+  modalToggle.setAttribute('aria-expanded', true);
+  bodyEl.classList.add('is-modal-open');
+  modalEl.classList.add('is-visible');
+  modalEl.querySelector('form input').focus();
+
+  const closeButton = modalEl.querySelector('#dialog-close');
+
+  closeButton.addEventListener('click', handleModalClose);
+});
+
+function handleModalClose(e) {
+  modalToggle.setAttribute('aria-expanded', false);
+  modalEl.classList.remove('is-visible');
+  bodyEl.classList.remove('is-modal-open');
+
+  e.target.removeEventListener('click', handleModalClose);
+}
